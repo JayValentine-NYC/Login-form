e49cb59d0684c1f76fc5bd7efef8dcd3d352b73f
Justin Valentine <justinvalentine31@gmail.com>
2023-04-12 16:34:47 -0400

    styling almost done need bigger image letter and auth

---
+++ script.js
@@ -0,0 +1,9 @@
+const span = document.querySelectorAll(span);
+const submitBtn = documtne.getElementById(submit);
+const form = document.querySelectorAll(input);
+
+function refresh(e){
+form.addEventListener('submit', ()=>{
+    e.preventDefault();
+})
+}
